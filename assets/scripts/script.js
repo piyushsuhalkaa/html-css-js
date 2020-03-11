c3aac14640484fa7666b33365519f934ac785662
piyushsuhalkaa <piyushsuhalkaa@gmail.com>
2020-03-11 17:52:01 +0530

    About Section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,5 @@
 /* On Scroll Header Change Script*/
+console.log("Connected");
 var header = document.getElementById("header");
 window.onscroll = function() {
   onScrollHeaderChange();
@@ -21,30 +22,3 @@ var showMenuItem = document.getElementsByClassName("navigation-links");
 function showMenu() {
   console.log("click Detected");
 }
-/* CTA Form Custom Select Option*/
-document
-  .querySelector(".form-item-select-wrapper")
-  .addEventListener("click", function() {
-    this.querySelector(".form-item-select").classList.toggle("open-option");
-  });
-
-for (const option of document.querySelectorAll(".item-select-option")) {
-  option.addEventListener("click", function() {
-    if (!this.classList.contains("item-option-selected")) {
-      this.parentNode
-        .querySelector(".item-select-option.item-option-selected")
-        .classList.remove("item-option-selected");
-      this.classList.add("item-option-selected");
-      this.closest(".form-item-select").querySelector(
-        ".item-select-trigger span"
-      ).textContent = this.textContent;
-    }
-  });
-}
-
-window.addEventListener("click", function(value) {
-  const select = document.querySelector(".form-item-select");
-  if (!select.contains(value.target)) {
-    select.classList.remove("open-option");
-  }
-});
